66b7f04783b5b8a1affb2b7f71a21177bb6701db
thang tran <thangtran3112@gmail.com>
2023-07-22 09:48:52 -0700

    Refactor Order components

--- src/components/OnlineOrder.js
+++ src/components/OnlineOrder.js
@@ -4,12 +4,12 @@ import React from 'react'
  * Do not use <Link> from React Dom, as it will not reload the contact page
  * Which would cause the Recaptcha to not showing up
  */
-const OrderPickup = () => {
+const OnlineOrder = ({linkAddress, description}) => {
     return (
-        <a href='https://www.clover.com/online-ordering/gong-cha-kelowna' className='btn'>
-            Order PickUp
+        <a href={linkAddress} className='btn'>
+            {description}
         </a>
     )
   }
   
-  export default OrderPickup
+  export default OnlineOrder
